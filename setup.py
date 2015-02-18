c0672a745078397a9fad5540d136c7b16130db0e
Jenny Qian <guiqinqian@gmail.com>
2015-02-18 10:39:08 -0600

    set version to 6.0.0 for branch tendenci6.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     name='tendenci-speakers',
     author='Schipul',
     author_email='programmers@schipul.com',
-    version='1.0.4',
+    version='6.0.0',
     license='GPL3',
     description='Speakers addon for Tendenci',
     long_description=longdesc,
@@ -27,5 +27,5 @@ setup(
     ],
     include_package_data=True,
     packages=find_packages(),
-    install_requires=['tendenci>=5.1'],
+    install_requires=['tendenci>=6.0.0'],
 )
